cc181deecd668831b07e559d96098bc1cc03e345
Kyle Zhu <jiacongzhu66@gmail.com>
2017-05-03 23:47:49 -0700

    Modified UI

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="aaabec1d-fb14-44b7-a391-cc808abc604d", modulePackage="com.kylezhudev.moveurcars", sdkRoot="C:\\Users\\Kyle\\AppData\\Local\\Android\\Sdk",layoutInfoDir="D:\\Google Drive\\Dev3\\moveURcars\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=19,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="dcf7dd2e-65d6-4787-ae8d-6f12dfa2e28b", modulePackage="com.kylezhudev.moveurcars", sdkRoot="C:\\Users\\Kyle\\AppData\\Local\\Android\\Sdk",layoutInfoDir="D:\\Google Drive\\Dev3\\moveURcars\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=19,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
